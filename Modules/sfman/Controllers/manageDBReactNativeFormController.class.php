06fa2c834721fec07345bd089a61db08b38afd22
Hadi AmirNahavandi <Hadi.nahavandi2010@gmail.com>
2019-12-01 10:35:09 +0330

    add makeReactNativeModuleRoute. complate makeReactNativeRoutes.

--- Modules/sfman/Controllers/manageDBReactNativeFormController.class.php
+++ Modules/sfman/Controllers/manageDBReactNativeFormController.class.php
@@ -11,59 +11,41 @@ namespace Modules\sfman\Controllers;
  */
 abstract class manageDBReactNativeFormController extends manageDBReactFormController
 {
-    protected function makeReactNativeRoutes($formInfo)
+    protected function makeReactNativeModuleRoute($formInfo)
     {
-        $trans = new Translator();
         $ModuleName = $formInfo['module']['name'];
         $FormName = $formInfo['form']['name'];
-        $FormNames = $FormName . "s";
-        $UFormNames = ucfirst($FormNames);
-        $UFormName = ucfirst($FormName);
-        $ModuleNames = $ModuleName . "s";
-        $FileName = $ModuleName . "_$FormName" . "List";
-
-        $AllFields = $this->getAllFormsOfFields();
-        $Fields = $AllFields['fields'];
-        $PersianFields = $AllFields['persianfields'];
-        $PureFields = $AllFields['purefields'];
-        $C = "// @flow
-import * as React from 'react';
-import { Link} from 'react-router-dom';
-import { FaEdit } from 'react-icons/fa';
-import { IoMdEye,IoMdAddCircle } from 'react-icons/io';
-import { MdDeleteForever } from 'react-icons/md';
-import SweetTable from '../../../../classes/sweet-table'
-import moment from 'moment-jalaali'
-import SweetFetcher from '../../../../classes/sweet-fetcher';
-import SweetAlert from '../../../../classes/SweetAlert';
-import Constants from '../../../../classes/Constants';
-import AccessManager from '../../../../classes/AccessManager';
-import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter,FormInline, Input } from 'mdbreact';
-import Common from '../../../../classes/Common';
-import SweetComponent from '../../../../classes/sweet-component';
-import SweetHttpRequest from '../../../../classes/sweet-http-request';
-
-class $FileName extends SweetComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            pages:1,
-            page:0,
-            canEdit:AccessManager.UserCan('$ModuleName','$FormName',AccessManager.EDIT),
-            canInsert:AccessManager.UserCan('$ModuleName','$FormName',AccessManager.INSERT),
-            canDelete:AccessManager.UserCan('$ModuleName','$FormName',AccessManager.DELETE),
-            displaySearchWindow:false,
-            ";
-        for ($i = 0; $i < count($Fields); $i++) {
-            if (FieldType::getFieldType($Fields[$i]) == FieldType::$FID)
-                $C = $C . "\r\n            $PureFields[$i]Options:[],";
-        }
-        $C .= "
-        };
-    };
+        $RouteFileName = $ModuleName . "_$FormName"."Routes";
+        $FileName = $ModuleName . "Routes";
+        $C = "
+import $RouteFileName from './$RouteFileName';
+    let $FileName=Object.assign({}, $RouteFileName);
+export default $FileName;
     ";
-        $DesignFile = $this->getReactCodeModuleDir() . "/modules/" . $ModuleName . "/pages/$FormName/" . $FileName . ".js";
+        $DesignFile = $this->getReactNativeCodeModuleDir() . "/modules/" . $ModuleName . "/routes/" . $FileName . ".js";
+        $this->SaveFile($DesignFile, $C);
+    }
+    protected function makeReactNativeRoutes($formInfo)
+    {
+        $this->makeReactNativeModuleRoute($formInfo);
+        $ModuleName = $formInfo['module']['name'];
+        $FormName = $formInfo['form']['name'];
+        $FileName = $ModuleName . "_$FormName"."Routes";
+        $ManageForm=$ModuleName."_"."$FormName"."Manage";
+        $ViewForm=$ModuleName."_"."$FormName"."View";
+        $ListForm=$ModuleName."_"."$FormName"."List";
+        $C = "
+import $ManageForm from '../pages/$FormName/$ManageForm';
+import $ViewForm from '../pages/$FormName/$ViewForm';
+import $ListForm from '../pages/$FormName/$ListForm';
+    let $FileName={
+        $ManageForm: $ManageForm,
+        $ViewForm: $ViewForm,
+        $ListForm: $ListForm,
+    };
+export default $FileName;
+    ";
+        $DesignFile = $this->getReactNativeCodeModuleDir() . "/modules/" . $ModuleName . "/routes/" . $FileName . ".js";
         $this->SaveFile($DesignFile, $C);
     }
 
